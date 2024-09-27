01b3a9a927ea7f817e6944680bb1e09272ad6a41
shaiyumg <shaiyumg@gmail.com>
2024-09-27 13:24:51 +0100

    Update App.js

--- App.js
+++ App.js
@@ -1,10 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
+export const App = () => {
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Hello world!</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -18,3 +19,5 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+
+export default App;
