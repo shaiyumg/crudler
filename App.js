e93947358128b8d2108b81ad368be1a04403c290
shaiyumg <shaiyumg@gmail.com>
2024-09-29 18:11:19 +0100

    Populating the ModuleListScreen
    Module list screen is now populated with dummy data, and alerts pop up when button is pressed. Screen is also now scrollable. names of the files have been Capitalised as I was getting warning errors on Expo Go that they should've been

--- App.js
+++ App.js
@@ -1,23 +1,46 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import ModuleListScreen from './src/components/screens/ModuleListScreen';
+import ModuleAddScreen from './src/components/screens/ModuleAddScreen';
+import ModuleViewScreen from './src/components/screens/ModuleViewScreen';
+import ModuleModifyScreen from './src/components/screens/ModuleModifyScreen';
 
-export const App = () => {
-
+const Stack = createNativeStackNavigator();
+ const App = () => {
   return (
-    <View style={styles.container}>
-      <Text>Hello world!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+    <NavigationContainer>
+    <Stack.Navigator 
+    initialRouteName='ModuleListScreen'
+    screenOptions={{
+      headerStyle: {backgroundColor: 'black'},
+      headerTintColor: 'white',
+    }}
+    >
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+    <Stack.Screen
+    name='ModuleListScreen'
+    component={ModuleListScreen}
+    options={{title: 'List modules'}}
+    />
+    <Stack.Screen
+    name='ModuleAddScreen'
+    component={ModuleAddScreen}
+    options={{title: 'Add module'}}
+    />
+    <Stack.Screen
+    name='ModuleViewScreen'
+    component={ModuleViewScreen}
+    options={{title: 'View modules'}}
+    />
+    <Stack.Screen
+    name='ModuleModifyScreen'
+    component={ModuleModifyScreen}
+    options={{title: 'Modify module'}}
+    />
+    
 
+    </Stack.Navigator>
+    </NavigationContainer>
+  )
+};
 export default App;
---
+++ src/components/layout/Screen.js
@@ -0,0 +1,21 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, View } from 'react-native';
+
+ const Screen = ({children}) => {
+  return (
+    <View style={styles.screen}>
+      {children}
+      <StatusBar style="light" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  screen: {
+    padding:15,
+    flex: 1,
+    backgroundColor: '#fff',
+  },
+});
+
+export default Screen;
