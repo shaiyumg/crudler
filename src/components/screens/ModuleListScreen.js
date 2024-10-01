98b3f6534dfa2cffd05c5d0a5d60e667a8f8e355
shaiyumg <shaiyumg@gmail.com>
2024-10-01 16:32:27 +0100

    Extracting reuseable components from ModuleListScreen (#5)
    Significant changes to ModuleListScreen with the addition of the modules folder with ModuleItem and ModuleList

--- src/components/screens/ModuleListScreen.js
+++ src/components/screens/ModuleListScreen.js
@@ -1,5 +1,6 @@
-import { Pressable, ScrollView, StyleSheet, Text, View} from 'react-native';
+import {StyleSheet} from 'react-native';
 import Screen from '../layout/Screen.js';
+import ModuleList from '../entity/modules/ModuleList.js';
 
 import initalModules from '../../data/modules.js';
 
@@ -7,35 +8,18 @@ import initalModules from '../../data/modules.js';
   ///Initialisations 
   const modules = initalModules;
   ///Handlers
-  const handleSelect =() => alert("Item selected")
+  const handleSelect =(module) => alert(`Item ${module.ModuleCode} selected`);
+
+  ///View
   return (
     <Screen>
-      <ScrollView style={styles.container}>
-      {modules.map((module)=>{
-          return(
-            <Pressable key={module.ModuleCode} onPress={handleSelect}>
-            <View  style={styles.item}>
-              <Text style={styles.text}>
-                {module.ModuleCode} {module.ModuleName}</Text>
-            </View>
-            </Pressable>
-          );
-        })}
-        </ScrollView>
+      <ModuleList modules={modules} onSelect={handleSelect}/>
     </Screen>
   );
 };
 
 const styles = StyleSheet.create({
   container:{},
-  item:{
-    paddingVertical: 15,
-    borderTopWidth: 1,
-    borderColor: 'lightgray',
-  },
-  text:{
-    fontSize: 16,
-  },
 });
 
 export default ModuleListScreen;
