3e3ffffb1e7656a5ca3202851704e3fbdd5d8665
shaiyumg <shaiyumg@gmail.com>
2024-10-06 21:28:41 +0100

    implementing handleDelete (#6)
    handleDelete function implemented and getting rid of alert function when pressing an element and instead will delete elements on press

--- src/components/screens/moduleListScreen.js
+++ src/components/screens/moduleListScreen.js
@@ -1,25 +1,27 @@
-import {StyleSheet} from 'react-native';
+import { useState } from 'react';
+import { StyleSheet } from 'react-native';
 import Screen from '../layout/Screen.js';
 import ModuleList from '../entity/modules/ModuleList.js';
 
-import initalModules from '../../data/modules.js';
+import initialModules from '../../data/modules.js';
+const ModuleListScreen = () => {
+  // Initializations
+  // State
+  const [modules, setModules] = useState(initialModules);
 
- const ModuleListScreen = () => {
-  ///Initialisations 
-  const modules = initalModules;
-  ///Handlers
-  const handleSelect =(module) => alert(`Item ${module.ModuleCode} selected`);
+  // Handlers
+  const handleDelete = (module) => {
+    setModules(modules.filter((item) => item.ModuleID !== module.ModuleID));
+  };
 
-  ///View
+  // View  
   return (
     <Screen>
-      <ModuleList modules={modules} onSelect={handleSelect}/>
+      <ModuleList modules={modules} onSelect={handleDelete} />
     </Screen>
   );
 };
 
-const styles = StyleSheet.create({
-  container:{},
-});
+const styles = StyleSheet.create({});
 
 export default ModuleListScreen;
