eb53fc723c53289c47d9c521ab8f45dc0e91c193
shaiyumg <shaiyumg@gmail.com>
2024-10-09 21:42:58 +0100

    Creating buttons and button tray
    Added modify and delete buttons with icons from vector-icons

--- src/components/entity/modules/ModuleView.js
+++ src/components/entity/modules/ModuleView.js
@@ -1,5 +1,7 @@
 import { StyleSheet, Text, View } from "react-native";
 import FullWidthImage from "react-native-fullwidth-image";
+import {Button, ButtonTray} from "../../UI/Button";
+import Icons from "../../UI/Icons";
 
 const ModuleView = ({ module }) => {
     return(
@@ -15,6 +17,15 @@ const ModuleView = ({ module }) => {
                 {module.ModuleLeaderName} <Text style = {styles.dimText}> (Module Leader)</Text>
               </Text>
             </View>
+            <ButtonTray>
+              <Button icon={<Icons.Edit />} label= 'Modify' />
+              <Button
+              icon = {<Icons.Delete />} 
+              label= 'Delete'
+              styleButton={{backgroundColor: 'mistyrose'}}
+              styleLabel={{color: 'red'}}
+              />
+              </ButtonTray>
           </View>
     );
 };
