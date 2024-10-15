583d8cca04253f98b4c75228b378b8242811b8d9
shaiyumg <shaiyumg@gmail.com>
2024-10-15 20:54:56 +0100

    Creating an add form (#11)
    Adding more functionality to the 'Add' button

--- src/components/screens/ModuleAddScreen.js
+++ src/components/screens/ModuleAddScreen.js
@@ -1,29 +1,63 @@
-import { StyleSheet, Text} from 'react-native';
+import { useState } from 'react';
+import { StyleSheet, Text, TextInput, View} from 'react-native';
 import Screen from '../layout/Screen';
 import { ButtonTray, Button } from '../UI/Button';
 import Icons from '../UI/Icons';
 
 const defaultModule = {
-  ModuleID: Math.floor(10000 + Math.random() * 9000),
-  ModuleCode: 'CI6330',
-  ModuleName: 'Mobile Application Development',
-  ModuleLevel: 6,
-  ModuleLeaderID: 1,
-  ModuleLeaderName: 'Graeme JONES',
-  ModuleImage: 'https://images.freeimages.com/images/small-previews/cf5/cellphone-1313194.jpg',
+  ModuleID:null,
+  ModuleCode: null,
+  ModuleName: null,
+  ModuleLevel: null,
+  ModuleLeaderID: null,
+  ModuleLeaderName: null,
+  ModuleImage: null, 
 };
 
  const ModuleAddScreen = ({navigation, route}) => {
   //Initialisations
   const {onAdd} = route.params;
-  //Handlers
-  const handleAdd = () => onAdd(defaultModule);
+  defaultModule.ModuleID = Math.floor(10000 + Math.random() * 9000),
+  defaultModule.ModuleImage = 'https://images.freeimages.com/images/small-previews/cf5/cellphone-1313194.jpg';
+ 
+  //States
+  const[module,setModule] = useState(defaultModule);
+
+  //Handlers  
+  const handleAdd = () => onAdd(module);
   const handleCancel = navigation.goBack;
 
+  const handleChange = (field,value)=> setModule({...module, [field]: value});
   //View
   return (
     <Screen>
-      <Text>Add</Text>
+      
+      <View style= {styles.item}>
+        <Text style= {styles.itemLabel}> Module code </Text>
+        <TextInput value={module.ModuleCode} onChangeText={(value)=>handleChange('ModuleCode',value)} style= {styles.itemTextInput} />
+      </View>
+
+      <View style= {styles.item}>
+        <Text style= {styles.itemLabel}> Module Name </Text>
+        <TextInput value={module.ModuleName} onChangeText={(value)=>handleChange('ModuleName',value)} style= {styles.itemTextInput} />
+      </View>
+
+      <View style= {styles.item}>
+        <Text style= {styles.itemLabel}> Module Level </Text>
+        <TextInput value={module.ModuleLevel} onChangeText={(value)=>handleChange('ModuleLevel',value)} style= {styles.itemTextInput} />
+      </View>
+
+      <View style= {styles.item}>
+        <Text style= {styles.itemLabel}> Module Leader </Text>
+        <TextInput value={module.ModuleLeaderName} onChangeText={(value)=>handleChange('ModuleLeaderName',value)} style= {styles.itemTextInput} />
+      </View>
+
+      <View style= {styles.item}>
+        <Text style= {styles.itemLabel}> Module Image URL </Text>
+        <TextInput value={module.ModuleImage} onChangeText={(value)=>handleChange('ModuleImage',value)} style= {styles.itemTextInput} />
+      </View>
+
+
     <ButtonTray>
       <Button label = 'Add' icon={<Icons.Add />} onClick={handleAdd}/>
       <Button label = 'Cancel' onClick={handleCancel} />
@@ -32,5 +66,20 @@ const defaultModule = {
   );
 };
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  itemLabel: {
+    color: 'grey',
+    fontSize: 16,
+    marginBottom: 5,
+  },
+  itemTextInput : {
+    height: 50,
+    paddingLeft: 10,
+    fontSize: 16,
+    backgroundColor: 'white',
+    borderRadius: 7,
+    borderWidth: 1,
+    borderColor: 'lightgray',
+  },
+})
 export default ModuleAddScreen;
